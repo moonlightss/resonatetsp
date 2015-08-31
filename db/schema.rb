e6d86766b0fb79ae12b0b18decca7ac40da97c9a
Simson Wu <simsonwu1992@gmail.com>
2015-08-31 17:21:56 +0800

    Add reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150829115819) do
+ActiveRecord::Schema.define(version: 20150831091123) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tutorial_centres", force: :cascade do |t|
     t.string   "name"
