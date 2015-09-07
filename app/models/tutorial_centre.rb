235f1f727e3758924d9f5e7ccd5aa95c0965e276
Simson Wu <simsonwu1992@gmail.com>
2015-09-07 19:45:11 +0800

    add data validations

--- app/models/tutorial_centre.rb
+++ app/models/tutorial_centre.rb
@@ -2,4 +2,12 @@ class TutorialCentre < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 
 	has_many :reviews
+
+	validates :name, :address, :phone, :website, :image, presence: true
+	validates :phone, format: { with: /\A\d{3}-\d{4}-\d{4}\z/,
+    message: "must be in the format 852-1234-5678" }
+     validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https://" }
+    
+
 end
